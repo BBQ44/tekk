70f17dcc001606f229537675cbfedb4fa7072f96
BBQ44 <42846778+BBQ44@users.noreply.github.com>
2018-08-31 14:06:08 +0300

    Update Tekir.py

--- Tekir.py
+++ Tekir.py
@@ -12,7 +12,32 @@ TOKEN = "NDIzODIyMjYyNjY4NjIzODcy.DZ--vQ.IKcVC8NiqlZbLVIxOCz7btxaUsg"
 client = Bot (command_prefix=BOT_PREFIX)
 
 print (discord.__version__)
+await client.send_message(message.channel, random.choice(possible_responses))
+@client.command(name='Avatar', pass_context=True)
+async def avatar(url, member: discord.Member = None):
+    embed = discord.Embed(title="Resim")
+    embed.set_image(url=member.avatar_url)
+    await client.say(embed=embed)
 
+
+killResponses = [" Yere Çakılarak Öldü " , " Elektrik Akımından Öldü ", " Silahla Vurularak Öldü " , " Kuduzdan Öldü" , " Kan Kaybından Öldü " , " Yaşlılıktan Öldü " , " Hastalıktan Öldü " , " Soğuk Esprilerden Öldü " , " Donarak Öldü " , " Aşırı Sıcaktan Öldü " , " Balinaların Yanına Atılarak Öldü" , "Death Note'ye Yazılarak Öldü"
+]
+@client.command(name='kill',
+                description="Açıklama",
+                pass_context = True)
+async def kill(ctx, *, member : discord.Member = None):
+    if member is None:
+        await client.say(ctx.message.author.mention + "Birini Etiketle")
+        return
+
+    if member.id == "423822262668623872":
+        await client.say(ctx.message.author.mention + " Beni Mi Öldürüceksin")
+    elif member.id == ctx.message.author:
+        await client.say(ctx.message.author.mention + "YAZI2")
+    else:
+        random.seed(time.time())
+        chosenResponse = killResponses[random.randrange(len(killResponses))]
+        await client.say("{}".format(member.name) + chosenResponse)
 @client.event
 async def on_ready():
     print ("Hazırım!!!")
@@ -118,30 +143,5 @@ async def on_message(message):
             ':fish: Palamut Tuttun',
             ':blowfish: Uskumru Tuttun',
         ]
-        await client.send_message(message.channel, random.choice(possible_responses))
-@client.command(name='Avatar', pass_context=True)
-async def avatar(url, member: discord.Member = None):
-    embed = discord.Embed(title="Resim")
-    embed.set_image(url=member.avatar_url)
-    await client.say(embed=embed)
-
-
-killResponses = [" Yere Çakılarak Öldü " , " Elektrik Akımından Öldü ", " Silahla Vurularak Öldü " , " Kuduzdan Öldü" , " Kan Kaybından Öldü " , " Yaşlılıktan Öldü " , " Hastalıktan Öldü " , " Soğuk Esprilerden Öldü " , " Donarak Öldü " , " Aşırı Sıcaktan Öldü " , " Balinaların Yanına Atılarak Öldü" , "Death Note'ye Yazılarak Öldü"
-]
-@client.command(name='kill',
-                description="Açıklama",
-                pass_context = True)
-async def kill(ctx, *, member : discord.Member = None):
-    if member is None:
-        await client.say(ctx.message.author.mention + "Birini Etiketle")
-        return
-
-    if member.id == "423822262668623872":
-        await client.say(ctx.message.author.mention + " Beni Mi Öldürüceksin")
-    elif member.id == ctx.message.author:
-        await client.say(ctx.message.author.mention + "YAZI2")
-    else:
-        random.seed(time.time())
-        chosenResponse = killResponses[random.randrange(len(killResponses))]
-        await client.say("{}".format(member.name) + chosenResponse)
+        
 client.run(TOKEN) 
