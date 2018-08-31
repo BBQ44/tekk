3e67c905c5b2ee21fe0ff7fac87e7bca0f032e24
BBQ44 <42846778+BBQ44@users.noreply.github.com>
2018-08-31 14:25:45 +0300

    Update Tekir.py

--- Tekir.py
+++ Tekir.py
@@ -7,7 +7,7 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 
 BOT_PREFIX = "tk!"
-TOKEN = "NDIzODIyMjYyNjY4NjIzODcy.DZ--vQ.IKcVC8NiqlZbLVIxOCz7btxaUsg"
+TOKEN = ""
 
 client = Bot (command_prefix=BOT_PREFIX)
 @client.event
